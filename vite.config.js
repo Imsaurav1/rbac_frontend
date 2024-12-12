372367ef727f8352b30254dfd704f4bf19fb08f8
Saurabh Kumar Jha <100080003+Imsaurav1@users.noreply.github.com>
2024-12-13 04:15:02 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// vite.config.js
+export default {
+  server: {
+    host: true, // Or use '0.0.0.0'
+    port: 5173, // Optional: Define a specific port
+  },
+};
